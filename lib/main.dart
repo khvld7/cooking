e15ff18c6d1102702638053b71921dd71cb0639d
khvld7 <khvl7d@gmail.com>
2024-04-25 00:01:29 +0300

    add launch screen and navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:cooking/components/style.dart';
 import 'package:cooking/onboarding/onboarding_widget.dart';
 import 'package:cooking/screens/screens.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -18,6 +26,15 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          titleSpacing: 24,
+          titleTextStyle: textStyleAppBar,
+          toolbarHeight: 200,
+        ),
+      ),
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: ScreenUtilInit(
           minTextAdapt: true,
           designSize: Size(360, 690),
